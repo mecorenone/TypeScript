d39dd93ed4481b334aa4dc69494883009287b663
Sheetal Nandi <shkamat@microsoft.com>
2023-10-11 14:49:05 -0700

    Some refactoring - Typing cache can maintain map from Project to Data instead of projectName as key

--- tests/baselines/reference/api/typescript.d.ts
+++ tests/baselines/reference/api/typescript.d.ts
@@ -3335,7 +3335,6 @@ declare namespace ts {
             readonly jsDocParsingMode: JSDocParsingMode | undefined;
             isKnownTypesPackageName(name: string): boolean;
             installPackage(options: InstallPackageOptions): Promise<ApplyCodeActionCommandResult>;
-            private get typingsCache();
             getCompilationSettings(): ts.CompilerOptions;
             getCompilerOptions(): ts.CompilerOptions;
             getNewLine(): string;
